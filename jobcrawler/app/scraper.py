a92355c8b3e618bb7ca45659edabadbd58840706
rahmani <a.rahmani@medrick.info>
2022-12-03 00:34:54 +0330

    jobinja project

---
+++ jobcrawler/app/scraper.py
@@ -0,0 +1,147 @@
+import json
+import re
+
+from bs4 import BeautifulSoup
+import requests
+
+__all__ = ['JobInjaScaper', ]
+
+
+def bs4_print(content, t=None):
+    if t == 'l':
+        return print(len(content))
+    return print(content.text)
+
+
+def bs4_return(content, cat=None):
+    content: str = content.text
+    content_ = content
+    if cat == 'title':
+        content_ = re.sub("(\(d*.*)", '', content)
+    content_ = content_.replace('  ', '').replace('\n', '')
+    if content_ == '':
+        content_ = content
+
+    return content_
+
+
+location_option = [
+    "تهران", "خراسان رضوی", "اصفهان", "البرز", "فارس", "قم", "آذربایجان شرقی", "مازندران", "گیلان", "کرمان", "خوزستان",
+    "یزد", "هرمزگان", "قزوین", "گلستان", "مرکزی", "بوشهر", "سمنان", "آذربایجان غربی", "اردبیل", "همدان",
+    "سیستان و بلوچستان", "کردستان", "زنجان", "کرمانشاه", "لرستان", "خراسان جنوبی", "چهارمحال بختیاری", "خراسان شمالی",
+    "ایلام", "کهکیلویه و بویراحمد",
+]
+
+
+class JobInjaScaper(object):
+    _job_inja = None
+
+    def __init__(self):
+        self.page_number = 0
+        super(JobInjaScaper, self).__init__()
+        self.session = requests.session()
+
+        self.base_url: str = 'https://jobinja.ir/'
+        self.filter_url: str = "jobs?"
+
+        self.locations: list = location_option
+        self.html_text = None
+        self.soup: [BeautifulSoup | None] = None
+        self.all_soup_pages: list = []
+        self.temp_file: list = []
+
+    @staticmethod
+    def job_inja():  # singleton
+        if not JobInjaScaper._job_inja:
+            JobInjaScaper._job_inja = JobInjaScaper()
+        return JobInjaScaper._job_inja
+
+    def _prepare_url(self, title: str = '', location: str = '', job_category: str = ''):
+        t_ = self.base_url + self.filter_url
+        t1 = f"filters[keywords][]={title}&"
+        t2 = f"filters[locations][]={location}&"
+        t3 = f"filters[job_categories][]={job_category}&"
+
+        return t_ + t1 + t2 + t3 + "b="
+
+    @staticmethod
+    def _soup_object(html):
+        return BeautifulSoup(html, 'lxml')
+
+    def send_first_request(self, title: str = '', location: str = '', job_category: str = ''):
+        self.html_text = self.session.get(
+            self._prepare_url(title=title, location=location, job_category=job_category)).content
+        self.soup = self._soup_object(html=self.html_text)
+        self._iterate_pages()
+        return True
+
+    def _paginator(self) -> list:
+        paginate = self.soup.find('div', class_='paginator')
+        pages = []
+        for l in paginate.ul:
+            tag_a = l.find('a')
+            if not (not tag_a or tag_a == -1):
+                pages.append(tag_a['href'])
+        page_link_sample = pages[0].split('page=')[0]
+        temp_page_number = [int(i.split('page=')[-1]) for i in sorted(pages)]
+
+        ma, mi = max(temp_page_number), min(temp_page_number)
+        return [page_link_sample + 'page=' + str(i) for i in range(mi, ma + 1)]
+
+    def _iterate_pages(self):
+        pages: list = self._paginator()
+        print("page number: ", end=' ')
+
+        self._calculate_page(sp=self.soup)
+
+        for p in pages:
+            self.all_soup_pages.append(self._soup_object(html=self.session.get(url=p, timeout=30).content))
+        for html_bs4 in self.all_soup_pages:
+            self._calculate_page(sp=html_bs4)
+        self._write_to_file(self.temp_file)
+
+    def _calculate_page(self, sp: BeautifulSoup):
+        print(self.page_number, end=' // ')
+        jobs = sp.find_all('div', class_='o-listView__itemWrap c-jobListView__itemWrap u-clearFix')
+        jobs_info: [list | dict] = []
+        for index, job in enumerate(jobs):
+            info: dict = {}
+
+            job_title_element = job.h3
+            job_title_a = job_title_element.a
+            job_title_link = job_title_a['href']
+            publish_time = job_title_element.span
+
+            info.update(
+                {
+                    'link': job_title_link,
+                    'title': bs4_return(job_title_element, cat='title'),
+                    'publish_time': bs4_return(publish_time)
+                })
+
+            # job info
+            li_tag = job.find_all('li', class_='c-jobListView__metaItem')
+
+            for j, il in enumerate(li_tag):
+                z = il.find('span')
+                if j == 0:
+                    info.update({'company': bs4_return(z)})
+                elif j == 1:
+                    info.update({'location': bs4_return(z)})
+                elif j == 2:
+                    info.update({'salary': bs4_return(z)})
+
+            jobs_info.append(info)
+        self.page_number += 1
+        self.temp_file.append(jobs_info)
+
+    @staticmethod
+    def _write_to_file(content: list[dict]):
+        print('====' * 5)
+        print(len(content))
+        with open('./my_file.json', mode='a', encoding='utf8') as f:
+            json.dump(content, f, indent=3, ensure_ascii=False)
+
+
+job_inja = JobInjaScaper.job_inja()
+job_inja.send_first_request(title='python')
