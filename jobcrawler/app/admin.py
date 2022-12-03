c137dad33ed72e98b827ff6e218ea8a957c0a362
alirahmani93 <ali93rahmani@gmail.com>
2022-12-03 09:54:27 +0330

    model and admin Job , Company Job detail

--- jobcrawler/app/admin.py
+++ jobcrawler/app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.db.transaction import atomic
+from django_object_actions import DjangoObjectActions
+
+from .models import Company, Job
 
 # Register your models here.
+
+admin.site.register(Company)
+
+
+@admin.register(Job)
+class JobAdmin(DjangoObjectActions, admin.ModelAdmin):
+    list_display = ['id', 'code', 'short_link', ]
+    readonly_fields = ['short_link', ]
+
+    actions = ['start_data']
+    change_list_template = 'admin/do.html'
+
+    def get_fields(self, request, obj=None):
+        fields = super(JobAdmin, self).get_fields(request, obj)
+        fields.remove('short_link')
+        return fields
+
+    @atomic
+    def start_data(self, request, queryset):
+        from .scraper import job_inja
+        job_inja.send_first_request(title='python')
--- jobcrawler/app/models.py
+++ jobcrawler/app/models.py
@@ -1,10 +1,59 @@
 from django.db import models
 
 
-class Job(models.Model):
+class BaseModel(models.Model):
+    is_active = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Company(BaseModel):
+    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    size = models.IntegerField(default=1)
+    category = models.IntegerField(default=1)
+    description = models.CharField(max_length=255, default='description')
+
+    def __str__(self):
+        return self.name
+
+
+class Job(BaseModel):
     title = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    company = models.CharField(max_length=255)
+    company = models.ForeignKey(to=Company, on_delete=models.CASCADE)
     salary = models.CharField(max_length=255)
     publish_time = models.CharField(max_length=255)
-    link = models.CharField(max_length=255)
+    link = models.URLField(max_length=255, )
+
+    code = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ['code', 'company']
+
+    @property
+    def short_link(self):
+        return self.link[:70]
+
+    def __str__(self):
+        return self.title
+
+
+class Skill(BaseModel):
+    name = models.CharField(max_length=255)
+
+
+class JobDetail(BaseModel):
+    job = models.OneToOneField(to=Job, on_delete=models.CASCADE)
+
+    category = models.CharField(max_length=255)
+    contract_type = models.CharField(max_length=255)
+    experience = models.CharField(max_length=255)
+    gender = models.IntegerField()
+    military = models.IntegerField()
+    education_degree = models.IntegerField()
+
+    skills = models.ManyToManyField(to=Skill, blank=True)
