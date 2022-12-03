030819258ccc5790679c14612c10e4cdf018a1ed
alirahmani93 <ali93rahmani@gmail.com>
2022-12-03 09:55:30 +0330

    settings and views and ...

--- jobcrawler/app/views.py
+++ jobcrawler/app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.shortcuts import redirect
+
 
 # Create your views here.
+def get_data(request):
+    if not request.user.is_superuser:
+        return HttpResponse("You are spammer")
+
+    from .scraper import job_inja
+    search = request.GET.get('search')
+    # job_inja.send_first_request(title='python')
+    redirect_to = request.GET.get('next', '')
+    if redirect_to:
+        return redirect(redirect_to)
+    else:
+        return HttpResponse("OK")
--- jobcrawler/jobcrawler/urls.py
+++ jobcrawler/jobcrawler/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-
+from app.views import get_data
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('get-data/', get_data),
 ]
